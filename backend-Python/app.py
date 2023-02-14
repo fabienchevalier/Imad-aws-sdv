d1eaf4f64d3a008e7e6bb16fdac57d05e33243e3
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2023-02-14 11:45:44 +0100

    up app

--- backend-Python/app.py
+++ backend-Python/app.py
@@ -40,33 +40,11 @@ employes = [
 ]
 
 @app.route('/api/v1/employees', methods=['GET'])
-def manage_products():
+def get_employe():
     if request.method == 'GET':
-        return jsonify({'products': products})
+        
 
-    if request.method == 'POST':
-        product = {
-            'id': products[-1]['id'] + 1,
-            'title': request.json['title'],
-            'description': request.json['description'],
-            'price': request.json['price'],
-            'image': request.json['image']
-        }
-        products.append(product)
-        return jsonify({'product': product}), 201
 
-@app.route('/product/<int:product_id>', methods=['GET', 'DELETE'])
-def manage_product(product_id):
-    product = [product for product in products if product['id'] == product_id]
-    if len(product) == 0:
-        return jsonify({'error': 'Product not found here'})
-
-    if request.method == 'GET':
-        return jsonify({'product': product[0]})
-
-    if request.method == 'DELETE':
-        products.remove(product[0])
-        return jsonify({'result': 'Product deleted'})
 
 if __name__ == '__main__':
     app.run(debug=True)
