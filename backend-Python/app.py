65b4a48406940599febfe4f7b858d99cf3046c5f
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2023-02-14 16:30:25 +0100

    persitent mongodb database

--- backend-Python/app.py
+++ backend-Python/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
+mongo_client = MongoClient()
 
 employees = [
     {
