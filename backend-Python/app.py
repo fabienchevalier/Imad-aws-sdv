bd4b2448f6ebb5e2faea368b3ac88bc930dc781e
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2023-02-15 09:56:21 +0100

    finish docker compose

--- backend-Python/app.py
+++ backend-Python/app.py
@@ -8,15 +8,6 @@ client = MongoClient('mongodb://root:example@mongo:27017/')
 db = client["employee_db"]
 col = db["employees"]
 
-# employees = [
-#     {
-#         'id': 1,
-#         'firstName': 'Pierre',
-#         'lastName': 'Da costa',
-#         'emailId': 'pierre@gmail.com'
-#     }
-# ]
-
 @app.route('/api/v1/employees', methods=['GET', 'POST'])
 def list_employee():
     if request.method == 'GET':
@@ -26,35 +17,51 @@ def list_employee():
                 db_list.append(i)
         return jsonify(db_list)
     
-    if request.method == 'POST':
+    if request.method == 'POST':    
+        db_list = []
+        cursor = col.find({}, {'_id': 0})
+        for i in cursor:
+                db_list.append(i)
         req_data = request.get_json()
+        req_data['id'] = db_list[-1]['id'] + 1
         col.insert_one(req_data).inserted_id
         return('', 204)
 
 @app.route('/api/v1/employees/<int:employee_id>', methods=['GET','DELETE', 'PUT'])
 def manage_employee(employee_id):
     if request.method == 'GET':
-        db_list = []
         cursor = col.find({}, {'_id': 0})
         for i in cursor:
-            db_list.append(i)
-        return jsonify(db_list['id'][f'{employee_id}'])
+            if i['id'] == employee_id:
+                return jsonify(i)
 
     if request.method == 'DELETE':
-        col.delete_one(
-            {
-                "id" : employee_id
-            }
-        )
+        cursor = col.find({}, {'_id': 0})
+        for i in cursor:
+            if i['id'] == employee_id:
+                col.delete_one(
+                    {
+                        'id' : employee_id
+                    }
+                )
         return jsonify()
     
     if request.method == 'PUT':
-        col.update_one({
-            "id" : employee_id,
-            'firstName': request.json['firstName'],
-            'lastName': request.json['lastName'],
-            'emailId': request.json['emailId']
-        })
-
+        col.update_one(
+            {
+            'id': employee_id
+            },    
+            {
+                "$set": 
+                    {
+                        "id" : employee_id,
+                        'firstName': request.json['firstName'],
+                        'lastName': request.json['lastName'],
+                        'emailId': request.json['emailId']
+                    }
+            }
+        )
+        return jsonify()
+        
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
