b211dffeaedf3a200e9b65b723bec8d2837771fd
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2023-02-16 14:22:03 +0100

    push new app and unit test

--- backend-Python/app.py
+++ backend-Python/app.py
@@ -4,34 +4,47 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
+
 client = MongoClient('mongodb://root:example@mongo:27017/')
-db = client["employee_db"]
-col = db["employees"]
+db = client.employee_db
+col = db.employees
 
 @app.route('/api/v1/employees', methods=['GET'])
-def get_employee():
-    return jsonify([i for i in col.find({}, {"_id":0})]), 200
+def get_employees():
+    employees = list(col.find({}, {'_id': 0}))
+    return jsonify(employees), 200
 
 @app.route('/api/v1/employees', methods=['POST'])
 def post_employee():
     data = request.get_json()
-    data["id"] = len([i for i in col.find({}, {"_id":0})]) + 1
+    employees = list(col.find({}, {'_id': 0}))
+    data['id'] = len(employees) + 1
     col.insert_one(data)
-    return jsonify(data), 200
+    return jsonify(), 200
 
 @app.route("/api/v1/employees/<int:id>", methods=['GET'])
-def get_employee_id(id):
-    return [element for element in [i for i in col.find({}, {"_id":0})] if element["id"] == id][0]
+def get_employee(id):
+    employee = col.find_one({'id': id}, {'_id': 0})
+    if employee is not None:
+        return jsonify(employee), 200
+    else:
+        return jsonify({'error': 'Employee not found'}), 404
 
 @app.route("/api/v1/employees/<int:id>", methods=['PUT'])
-def put_employee_id(id):
-    result = col.update_one({"id": int(id)}, {"$set": request.get_json()})
-    return jsonify({"result": "Employee updated"}) if result.modified_count == 1 else jsonify({"error": "Employee not found"}) 
+def put_employee(id):
+    result = col.update_one({'id': id}, {'$set': request.get_json()})
+    if result.modified_count == 1:
+        return jsonify({'result': 'Employee updated'}), 200
+    else:
+        return jsonify({'error': 'Employee not found'}), 404
 
 @app.route("/api/v1/employees/<int:id>", methods=['DELETE'])
-def del_employee_id(id):
-    result = col.delete_one({'id': int(id)})
-    return jsonify({'result': 'Employee deleted'}) if result.deleted_count == 1 else jsonify({'error': 'Employee not found'})
+def delete_employee(id):
+    result = col.delete_one({'id': id})
+    if result.deleted_count == 1:
+        return jsonify({'result': 'Employee deleted'}), 200
+    else:
+        return jsonify({'error': 'Employee not found'}), 404
     
 if __name__ == '__main__':
     app.run(debug=True)
