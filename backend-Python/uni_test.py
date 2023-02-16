3786a0160eb913a369e3b939d7269e7c8cb96016
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2023-02-16 16:29:38 +0100

    code cleaning

---
+++ backend-Python/uni_test.py
@@ -0,0 +1,37 @@
+import unittest
+import requests
+
+class TestEmployeeAPI(unittest.TestCase):
+    
+    base_url = "http://localhost:8080/api/v1"
+    
+    def test_get_employees(self):
+        response = requests.get(f"{self.base_url}/employees")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+    
+    def test_add_employee(self):
+        new_employee = {
+            "firstName": "TEST",
+            "lastName": "SUBJECT",
+            "emailId": "TEST@TEST.FR"
+        }
+        response = requests.post(f"{self.base_url}/employees", json=new_employee)
+        id_post = response['id']
+        return id_post
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), dict)
+    
+    def test_update_employee(self):
+        updated_employee = {
+            "firstName": "up_TEST",
+            "lastName": "up_SUBJECT"
+        }
+        employee_id = id_post
+        response = requests.put(f"{self.base_url}/employees/{employee_id}", json=updated_employee)
+        self.assertEqual(response.status_code, 204)
+    
+    def test_delete_employee(self):
+        employee_id = 1
+        response = requests.delete(f"{self.base_url}/employees/{employee_id}")
+        self.assertEqual(response.status_code, 204)
